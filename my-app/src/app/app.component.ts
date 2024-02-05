aa5ec8c2e2907e3b5e50318aaae5022a5570e523
Julia Izbrodina <94717941+JuliaAvona@users.noreply.github.com>
2024-02-05 12:31:17 -0600

    feat: 3-practicing-directives

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
+import { timestamp } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styles: [`
-  h3 {
-  color: rgb(30, 128, 241);
-}
+    h3 {
+    color: rgb(30, 128, 241);
+  }
+
+    .blue {
+      background-color: blue;
+      color: white;
+    }
+
+    .white {
+      background-color: white;
+      color: black;
+    }
   `]
 })
 export class AppComponent {
   title = 'my-app';
+  isDisplayed = true;
+  clicksRegistered: number[] = [];
+  styleStatus = 'white';
+
+  constructor() {
+  }
+
+  displayDetails() {
+    this.isDisplayed === true ? this.isDisplayed = false : this.isDisplayed = true;
+    const timestamp: number = new Date().getTime();
+    this.clicksRegistered.push(timestamp);
+    console.log('timestamp ' + timestamp);
+  }
+
+  getColor() {
+   this.clicksRegistered.length > 4 ? this.styleStatus = 'blue' : this.styleStatus = 'white';
+  }
 }
