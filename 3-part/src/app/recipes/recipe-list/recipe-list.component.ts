3b500ed824774ca45af53d34cb45f4a54b736aa3
Julia Izbrodina <jizbrodina@positrace.com>
2024-02-18 14:56:41 -0600

    feat: recipe.model

--- 3-part/src/app/recipes/recipe-list/recipe-list.component.ts
+++ 3-part/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.freeiconspng.com/thumbs/recipes-icon-png/recipe-icon--16.png'),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://www.freeiconspng.com/thumbs/recipes-icon-png/recipe-icon--16.png')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+}
 }
